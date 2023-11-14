a5a3f4e42ad063d66792e7aea370886e4e79a5a6
Mustafasohail7 <mustufasohail7@gmail.com>
2023-11-15 01:26:21 +0500

    initial setup + get_patch()

---
+++ terrain.js
@@ -0,0 +1,33 @@
+const get_patch = (xmin,xmax,zmin,zmax) => {
+    x = xmax-xmin
+    z = zmax-zmin
+    x_resolution = 100;z_resolution = 100
+    step_x = x/x_resolution
+    step_z = z/z_resolution
+    vertices = []
+    indices = []
+    
+    currentVertex = 0
+    for(var i=xmin;i<xmax;i+=step_x){
+        for(var j=zmin;j<zmax;j+=step_z){
+            const pos = vec3(i,0,j)
+            //calculate normal here
+            // const normal = vec3(0,1,0)
+            
+            vertices.push(pos)
+
+            indices.push(currentVertex)
+            indices.push(currentVertex+1)
+            indices.push(currentVertex+x_resolution)
+            indices.push(currentVertex+1)
+            indices.push(currentVertex+x_resolution+1)
+            indices.push(currentVertex+x_resolution)
+
+            currentVertex++
+        }
+    }
+    console.log(vertices)
+    console.log(indices)
+}
+
+get_patch(0,1,0,1)
